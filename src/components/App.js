433346c0ec4536ecbaf435f33db9be3f4c9bda5c
Albin SAj Chalissery <albinsaju1234@gmail.com>
2021-08-08 13:18:48 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">NetFlix</h1>
+      <br/>
+      <h4 data-ns-test="project-name">video streaming web site</h4>
+    </div>
   )
 }
 
